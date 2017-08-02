30c1829c5150ede23c7428ee39b5ddd711d4d77f
Recardo Poole <recardo.poole@gmail.com>
2017-08-01 18:10:17 -0600

    updated put

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const cors = require('cors')
 var port = process.env.PORT || 8080
 var app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false}))
 
 
-app.use(cors())
 app.use('/', router)
 
 
